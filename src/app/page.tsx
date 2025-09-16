52beb78b64a6ca833e1e8fee39eeb8d8db41fcad
pradeept <pradeep.tarakar17@gmail.com>
2025-09-16 11:43:04 +0530

    add github icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import FormComponent from "@/components/formComponent";
+import { IconBrandGithub } from "@tabler/icons-react";
 import React from "react";
 
 async function page() {
   return (
-    <main className='min-h-screen w-full bg-gradient-to-br from-blue-100 via-white to-blue-200 flex flex-col items-center justify-start py-10 px-4'>
+    <main className='relative min-h-screen w-full bg-gradient-to-br from-blue-100 via-white to-blue-200 flex flex-col items-center justify-start py-10 px-4'>
       <div className='w-full max-w-2xl mx-auto flex flex-col items-center'>
         <div className='mb-8 text-center'>
           <h1 className='text-4xl md:text-5xl font-extrabold  drop-shadow-lg mb-2 tracking-tight'>
@@ -13,6 +14,14 @@ async function page() {
             Powered by Langchain and Perplexity
           </p>
         </div>
+        <a
+          href='https://github.com/pradeept/Chatbot-Langchain'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='bg-slate-200 rounded p-1 absolute right-5 md:right-10'
+        >
+          <IconBrandGithub size={32} />
+        </a>
         <FormComponent />
       </div>
     </main>
